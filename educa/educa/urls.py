4f8930439e2fcf5128b0e320f9c1cf30d9ae9d92
Divyesh Parikh <dcparikh360@gmail.com>
2025-03-06 10:56:33 +0530

    Added CourseList View

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from courses.views import CourseListView
 
 urlpatterns = [
     path(
@@ -13,7 +14,7 @@ urlpatterns = [
     ),
     path('admin/', admin.site.urls),
     path('course/', include('courses.urls')),
-    
+    path('', CourseListView.as_view(), name='course_list'),
 ]
 
 if settings.DEBUG:
