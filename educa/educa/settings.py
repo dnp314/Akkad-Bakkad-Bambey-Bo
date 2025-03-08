adf0dc66df876b709683a128c04d0aff841a47da
Divyesh Parikh <dcparikh360@gmail.com>
2025-03-08 16:36:48 +0530

    Added ViewSets

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
 }
 
 CACHES ={
-    'deafult': {
+    'default': {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
         'LOCATION': 'redis://127.0.0.1:6379',
     }
