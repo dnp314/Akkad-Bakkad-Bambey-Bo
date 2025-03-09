4d969affd2e66a2d6ff30b13753d4ff95144da0a
Divyesh Parikh <dcparikh360@gmail.com>
2025-03-09 10:41:18 +0530

    Added Authentication for APIs

--- educa/courses/api/urls.py
+++ educa/courses/api/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
   path('',include(router.urls)),
   # path('subjects/',views.SubjectListView.as_view(),name='subject_list'),
   # path('subjects/<pk>/',views.SubjectDetailView.as_view(),name='subject_detail'),
+  path('courses/<pk>/enroll/', views.CourseEnrollView.as_view(), name='course_enroll'),
 ]
--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,6 +1,11 @@
 from django.db.models import Count
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from courses.api.pagination import StandardPagination
 from courses.api.serializers import CourseSerializer, SubjectSerializer
 from courses.models import Course, Subject
@@ -23,3 +28,14 @@ class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Subject.objects.annotate(total_courses=Count('courses'))
     serializer_class = SubjectSerializer
     pagination_class = StandardPagination
+    
+
+class CourseEnrollView(APIView):
+    authentication_classes = [BasicAuthentication]
+    permission_classes     = [IsAuthenticated]
+    
+    def post(self, request, pk, format=None):
+        course =  get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
+    
